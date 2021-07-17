a103bcf9271baf3581bd123fe90973cf49944d6d
Ashancy <ashrikan@gmail.com>
2021-07-17 09:21:54 +0300

    Bootstrap grid

---
+++ forms.php
@@ -0,0 +1,87 @@
+
+<!doctype html>
+<html>
+<head>
+    <title>
+        Forms
+    </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+    <!-- jQuery library -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Popper JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <!-- Latest compiled JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+1 - 12
+<div class="row">
+    <div class="col-md-1 bg-primary text-white">
+        Col m1
+    </div>
+    <div class="col-md-1 bg-secondary">
+        Col m1
+    </div>
+    <div class="col-md-1 bg-success">
+        Col m1
+    </div>
+    <div class="col-md-1 bg-info">
+        Col m1
+    </div>
+    <div class="col-md-1 bg-muted">
+        Col m1
+    </div>
+    <div class="col-md-1 bg-dark">
+        Col m1
+    </div>
+    <div class="col-md-1 bg-warning">
+        Col m1
+    </div>
+    <div class="col-md-1">
+        Col m1
+    </div>
+    <div class="col-md-1">
+        Col m1
+    </div>
+    <div class="col-md-1">
+        Col m1
+    </div>
+    <div class="col-md-1">
+        Col m1
+    </div>
+    <div class="col-md-1 bg-danger">
+        Col m1
+    </div>
+    <div class="col-md-7 bg-danger">
+        Col 7
+    </div>
+    <div class="col-md-5 bg-warning"></div>
+    <div class="col-md-3 bg-success">
+        Col 3
+    </div>
+    <div class="col-md-3 bg-info">
+        Col 3
+    </div>
+    <div class="col-md-3 bg-warning">
+        Col md 3
+    </div>
+    <div class="col-md-3 text-white bg-dark">
+        Col md 3
+    </div>
+    <div class="col-md-5 bg-success">
+        5
+    </div>
+    <div class="col-md-5 bg-danger">
+        5
+    </div>
+    <div class="col-md-2 bg-warning">
+        2
+    </div>
+</div>
+</body>
+</html>
