793e7fcda6233aa6877e2ea712fff3138017f274
Ashancy <ashrikan@gmail.com>
2021-07-17 10:32:13 +0300

    new changes with php class

---
+++ phpclass.php
@@ -0,0 +1,23 @@
+<?php
+
+$first_name = "Emilly"; //String
+
+$last_name = "Nakachwa"; //String
+
+$age = 20;
+
+$ashan_age = 26; //Integer
+
+echo $first_name;
+echo $last_name;
+echo $age;
+
+
+echo "<br/>";
+
+echo $ashan_age / $age; //Float value
+
+
+
+//if statement
+
